fc373e45dbb8ad6c1907b6c4208c5085054f907a
James <mr.jamesdickinson@gmail.com>
2020-10-24 23:07:18 +0100

    Phishing
    Phishing

---
+++ src/Phishing.py
@@ -0,0 +1,37 @@
+from tkinter import *
+import tkinter as tk
+from src.base_gui import BaseGUI
+
+class Phishing(BaseGUI):
+    def __init__(self, master_window, controller):
+        #  calling parent constructor:
+        super(Phishing, self).__init__(master_window, controller, "Third Frame")
+
+
+        # This is for the background
+        self.background_image = PhotoImage(file='phishing.png')
+        self.background_label = Label(master_window, image=self.background_image)
+        self.background_label.place(x=0, y=0, relwidth=1, relheight=1)
+
+        # This adds another button below saying Facebook, just if they didn't click on the image
+        # This still needs a command
+        self.aButton = Button(self, text='A', font=(
+            'Verdana', 15), command=self.on_button_click).place(x=5, y=550)
+        self.bButton = Button(self, text='B', font=(
+            'Verdana', 15), command=self.on_button_click).place(x=40, y=550)
+        self.cbButton = Button(self, text='C', font=(
+            'Verdana', 15), command=self.on_button_click).place(x=75, y=550)
+
+    def on_button_click(self):
+        # This switches to SecondFrame
+        self.controller.switch_frame("bubble_end")
+
+
+if __name__ == "__main__":
+    from src.controller import Controller
+
+    # Any testing specific to this class should be done here
+    window = Tk()
+    app = Controller(window, Phishing)
+    app.start()
+    window.mainloop()
