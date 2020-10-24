16515ec2c02614b4db6e5e63b9b7be2c11a67f6f
AnnaClee <67109502+AnnaClee@users.noreply.github.com>
2020-10-24 23:16:10 +0100

    Create login2.py

---
+++ src/login2.py
@@ -0,0 +1,42 @@
+from tkinter import *
+from functools import partial
+from src.base_gui import BaseGUI
+
+
+
+class PasswordGUI(BaseGUI):
+    def __init__(self, master, controller):
+        super(PasswordGUI, self).__init__(master,controller, "User Login")
+
+        self.label = Label(master, text="Username").grid(row=0)
+        username = StringVar()
+        self.username_entry = Entry(master, textvariable=username).grid(row=0, column=1)
+
+        self.passwordLabel = Label(master, text="Password").grid(row=1, column=0)
+        entered_password = StringVar()
+        self.passwordEntry = Entry(master, textvariable=entered_password, show='*').grid(row=1,column=1)
+        validateLogin = partial(self.validate_login, username, entered_password)
+
+        self.login_button = Button(master, text="login", command=validateLogin).grid(row=4, column=0)
+
+        self.close_button = Button(master, text="Close", command=master.quit)
+
+
+    def validate_login(self, username, entered_password):
+        password = "remember"
+        if entered_password.get() == password:
+            self.controller.switch_frame('image')
+        else:
+            self.controller.switch_frame('phishing')
+
+
+if __name__ == "__main__":
+    root = Tk()
+    root.config(bg="white")
+    canvas = Canvas(root, width=1024, height=576, bg="white")
+    canvas.grid(row=0, column=0, columnspan=3)
+
+    # Any testing specific to this class should be done here
+    window1 = Tk()  # initialize the window manager with the tkinter.Tk() method and assign it to a variable
+    PasswordGUI(window1)
+    window1.mainloop()
